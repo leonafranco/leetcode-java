ab5afe096952bcab1b6184d77486b55f3d141bb7
leonafranco <leonardomf96@gmail.com>
2024-03-03 23:21:14 +0000

    add binary search

--- src/main/java/org/example/Q704BinarySearch.java
+++ src/main/java/org/example/Q704BinarySearch.java
@@ -1,2 +1,26 @@
-package org.example;public class Q704BinarySearch {
+package org.example;
+
+public class Q704BinarySearch {
+    class Solution {
+        public int search(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length-1;
+            int mid = 0;
+            while(left <= right) {
+                mid = (left + (right - left)/2);
+                if(nums[mid] > target) {
+                    right = mid - 1;
+                }
+                if(nums[mid] < target) {
+                    left = mid + 1;
+                }
+                if(nums[mid] == target){
+                    return mid;
+                }
+            }
+            return -1;
+        }
+
+        // 1 2 3 4 5 6 7 8 9 10 11
+    }
 }
